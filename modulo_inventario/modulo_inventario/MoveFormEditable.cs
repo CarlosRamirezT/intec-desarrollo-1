56f6d2d8566e2c114376aac001594150b684d341
Frank Hartling <feduardohg@gmail.com>
2021-07-05 19:09:22 -0400

    Se agrego la lista del editable del move

---
+++ modulo_inventario/modulo_inventario/MoveFormEditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modulo_inventario
+{
+    public partial class MoveFormEditable : Form
+    {
+        public MoveFormEditable()
+        {
+            InitializeComponent();
+        }
+    }
+}
