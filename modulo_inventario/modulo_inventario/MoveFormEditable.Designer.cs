16c00c089fd3d4b002fdeacbc6b00521df5199c2
Frank Hartling <feduardohg@gmail.com>
2021-07-05 19:21:26 -0400

    Correcion de los codigos del move

--- modulo_inventario/modulo_inventario/MoveFormEditable.Designer.cs
+++ modulo_inventario/modulo_inventario/MoveFormEditable.Designer.cs
@@ -29,12 +29,217 @@ namespace modulo_inventario
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.labelDireccion = new System.Windows.Forms.Label();
+            this.labelTipo = new System.Windows.Forms.Label();
+            this.labelOrigen = new System.Windows.Forms.Label();
+            this.labelDestino = new System.Windows.Forms.Label();
+            this.labelFecha = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxContacto = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.textBoxTipo = new System.Windows.Forms.TextBox();
+            this.textBoxOrigen = new System.Windows.Forms.TextBox();
+            this.textBoxDestino = new System.Windows.Forms.TextBox();
+            this.textBoxFecha = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(28, 13);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuardar.TabIndex = 0;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(134, 13);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 1;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 208);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(743, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(52, 65);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 15);
+            this.labelID.TabIndex = 3;
+            this.labelID.Text = "ID";
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Location = new System.Drawing.Point(52, 123);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(56, 15);
+            this.labelContact.TabIndex = 4;
+            this.labelContact.Text = "Contacto";
+            this.labelContact.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelDireccion
+            // 
+            this.labelDireccion.AutoSize = true;
+            this.labelDireccion.Location = new System.Drawing.Point(52, 180);
+            this.labelDireccion.Name = "labelDireccion";
+            this.labelDireccion.Size = new System.Drawing.Size(57, 15);
+            this.labelDireccion.TabIndex = 5;
+            this.labelDireccion.Text = "Dirección";
+            // 
+            // labelTipo
+            // 
+            this.labelTipo.AutoSize = true;
+            this.labelTipo.Location = new System.Drawing.Point(318, 65);
+            this.labelTipo.Name = "labelTipo";
+            this.labelTipo.Size = new System.Drawing.Size(30, 15);
+            this.labelTipo.TabIndex = 6;
+            this.labelTipo.Text = "Tipo";
+            // 
+            // labelOrigen
+            // 
+            this.labelOrigen.AutoSize = true;
+            this.labelOrigen.Location = new System.Drawing.Point(318, 123);
+            this.labelOrigen.Name = "labelOrigen";
+            this.labelOrigen.Size = new System.Drawing.Size(43, 15);
+            this.labelOrigen.TabIndex = 7;
+            this.labelOrigen.Text = "Origen";
+            // 
+            // labelDestino
+            // 
+            this.labelDestino.AutoSize = true;
+            this.labelDestino.Location = new System.Drawing.Point(318, 180);
+            this.labelDestino.Name = "labelDestino";
+            this.labelDestino.Size = new System.Drawing.Size(47, 15);
+            this.labelDestino.TabIndex = 8;
+            this.labelDestino.Text = "Destino";
+            // 
+            // labelFecha
+            // 
+            this.labelFecha.AutoSize = true;
+            this.labelFecha.Location = new System.Drawing.Point(529, 65);
+            this.labelFecha.Name = "labelFecha";
+            this.labelFecha.Size = new System.Drawing.Size(38, 15);
+            this.labelFecha.TabIndex = 9;
+            this.labelFecha.Text = "Fecha";
+            this.labelFecha.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(134, 62);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 23);
+            this.textBoxID.TabIndex = 10;
+            // 
+            // textBoxContacto
+            // 
+            this.textBoxContacto.Location = new System.Drawing.Point(134, 120);
+            this.textBoxContacto.Name = "textBoxContacto";
+            this.textBoxContacto.Size = new System.Drawing.Size(100, 23);
+            this.textBoxContacto.TabIndex = 11;
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Location = new System.Drawing.Point(134, 170);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(100, 23);
+            this.textBoxDireccion.TabIndex = 12;
+            // 
+            // textBoxTipo
+            // 
+            this.textBoxTipo.Location = new System.Drawing.Point(392, 62);
+            this.textBoxTipo.Name = "textBoxTipo";
+            this.textBoxTipo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxTipo.TabIndex = 13;
+            // 
+            // textBoxOrigen
+            // 
+            this.textBoxOrigen.Location = new System.Drawing.Point(392, 120);
+            this.textBoxOrigen.Name = "textBoxOrigen";
+            this.textBoxOrigen.Size = new System.Drawing.Size(100, 23);
+            this.textBoxOrigen.TabIndex = 14;
+            // 
+            // textBoxDestino
+            // 
+            this.textBoxDestino.Location = new System.Drawing.Point(392, 170);
+            this.textBoxDestino.Name = "textBoxDestino";
+            this.textBoxDestino.Size = new System.Drawing.Size(100, 23);
+            this.textBoxDestino.TabIndex = 15;
+            // 
+            // textBoxFecha
+            // 
+            this.textBoxFecha.Location = new System.Drawing.Point(608, 62);
+            this.textBoxFecha.Name = "textBoxFecha";
+            this.textBoxFecha.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFecha.TabIndex = 16;
+            // 
+            // MoveFormEditable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxFecha);
+            this.Controls.Add(this.textBoxDestino);
+            this.Controls.Add(this.textBoxOrigen);
+            this.Controls.Add(this.textBoxTipo);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.textBoxContacto);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelFecha);
+            this.Controls.Add(this.labelDestino);
+            this.Controls.Add(this.labelOrigen);
+            this.Controls.Add(this.labelTipo);
+            this.Controls.Add(this.labelDireccion);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Name = "MoveFormEditable";
             this.Text = "MoveFormEditable";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelContact;
+        private System.Windows.Forms.Label labelDireccion;
+        private System.Windows.Forms.Label labelTipo;
+        private System.Windows.Forms.Label labelOrigen;
+        private System.Windows.Forms.Label labelDestino;
+        private System.Windows.Forms.Label labelFecha;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxContacto;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.TextBox textBoxTipo;
+        private System.Windows.Forms.TextBox textBoxOrigen;
+        private System.Windows.Forms.TextBox textBoxDestino;
+        private System.Windows.Forms.TextBox textBoxFecha;
     }
 }
