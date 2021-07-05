aca0b661a3059d2db169da450b33d0c66f2c5e6a
Frank Hartling <feduardohg@gmail.com>
2021-07-05 19:14:50 -0400

    Codigos faltantes del move

--- modulo_inventario/modulo_inventario/MoveFormInformation.Designer.cs
+++ modulo_inventario/modulo_inventario/MoveFormInformation.Designer.cs
@@ -29,12 +29,241 @@ namespace modulo_inventario
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelNombreContent = new System.Windows.Forms.Label();
+            this.labelContacto = new System.Windows.Forms.Label();
+            this.labelContactoContent = new System.Windows.Forms.Label();
+            this.LabelDireccion = new System.Windows.Forms.Label();
+            this.labelDireccionContent = new System.Windows.Forms.Label();
+            this.labelTipo = new System.Windows.Forms.Label();
+            this.labelTipoContent = new System.Windows.Forms.Label();
+            this.labelOrigen = new System.Windows.Forms.Label();
+            this.labelOrigenContent = new System.Windows.Forms.Label();
+            this.labelDestino = new System.Windows.Forms.Label();
+            this.labelDestinoContent = new System.Windows.Forms.Label();
+            this.labelFecha = new System.Windows.Forms.Label();
+            this.labelFechaContent = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 243);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(689, 168);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(713, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(115, 12);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(29, 73);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(18, 15);
+            this.labelNombre.TabIndex = 4;
+            this.labelNombre.Text = "ID";
+            // 
+            // labelNombreContent
+            // 
+            this.labelNombreContent.AutoSize = true;
+            this.labelNombreContent.Location = new System.Drawing.Point(140, 72);
+            this.labelNombreContent.Name = "labelNombreContent";
+            this.labelNombreContent.Size = new System.Drawing.Size(38, 15);
+            this.labelNombreContent.TabIndex = 5;
+            this.labelNombreContent.Text = "label2";
+            // 
+            // labelContacto
+            // 
+            this.labelContacto.AutoSize = true;
+            this.labelContacto.Location = new System.Drawing.Point(29, 120);
+            this.labelContacto.Name = "labelContacto";
+            this.labelContacto.Size = new System.Drawing.Size(56, 15);
+            this.labelContacto.TabIndex = 6;
+            this.labelContacto.Text = "Contacto";
+            // 
+            // labelContactoContent
+            // 
+            this.labelContactoContent.AutoSize = true;
+            this.labelContactoContent.Location = new System.Drawing.Point(140, 120);
+            this.labelContactoContent.Name = "labelContactoContent";
+            this.labelContactoContent.Size = new System.Drawing.Size(38, 15);
+            this.labelContactoContent.TabIndex = 7;
+            this.labelContactoContent.Text = "label4";
+            // 
+            // LabelDireccion
+            // 
+            this.LabelDireccion.AutoSize = true;
+            this.LabelDireccion.Location = new System.Drawing.Point(28, 169);
+            this.LabelDireccion.Name = "LabelDireccion";
+            this.LabelDireccion.Size = new System.Drawing.Size(57, 15);
+            this.LabelDireccion.TabIndex = 8;
+            this.LabelDireccion.Text = "Dirección";
+            // 
+            // labelDireccionContent
+            // 
+            this.labelDireccionContent.AutoSize = true;
+            this.labelDireccionContent.Location = new System.Drawing.Point(140, 169);
+            this.labelDireccionContent.Name = "labelDireccionContent";
+            this.labelDireccionContent.Size = new System.Drawing.Size(38, 15);
+            this.labelDireccionContent.TabIndex = 9;
+            this.labelDireccionContent.Text = "label2";
+            // 
+            // labelTipo
+            // 
+            this.labelTipo.AutoSize = true;
+            this.labelTipo.Location = new System.Drawing.Point(335, 72);
+            this.labelTipo.Name = "labelTipo";
+            this.labelTipo.Size = new System.Drawing.Size(30, 15);
+            this.labelTipo.TabIndex = 10;
+            this.labelTipo.Text = "Tipo";
+            // 
+            // labelTipoContent
+            // 
+            this.labelTipoContent.AutoSize = true;
+            this.labelTipoContent.Location = new System.Drawing.Point(477, 71);
+            this.labelTipoContent.Name = "labelTipoContent";
+            this.labelTipoContent.Size = new System.Drawing.Size(38, 15);
+            this.labelTipoContent.TabIndex = 11;
+            this.labelTipoContent.Text = "label4";
+            // 
+            // labelOrigen
+            // 
+            this.labelOrigen.AutoSize = true;
+            this.labelOrigen.Location = new System.Drawing.Point(335, 120);
+            this.labelOrigen.Name = "labelOrigen";
+            this.labelOrigen.Size = new System.Drawing.Size(43, 15);
+            this.labelOrigen.TabIndex = 12;
+            this.labelOrigen.Text = "Origen";
+            // 
+            // labelOrigenContent
+            // 
+            this.labelOrigenContent.AutoSize = true;
+            this.labelOrigenContent.Location = new System.Drawing.Point(477, 120);
+            this.labelOrigenContent.Name = "labelOrigenContent";
+            this.labelOrigenContent.Size = new System.Drawing.Size(38, 15);
+            this.labelOrigenContent.TabIndex = 13;
+            this.labelOrigenContent.Text = "label2";
+            // 
+            // labelDestino
+            // 
+            this.labelDestino.AutoSize = true;
+            this.labelDestino.Location = new System.Drawing.Point(335, 169);
+            this.labelDestino.Name = "labelDestino";
+            this.labelDestino.Size = new System.Drawing.Size(47, 15);
+            this.labelDestino.TabIndex = 14;
+            this.labelDestino.Text = "Destino";
+            // 
+            // labelDestinoContent
+            // 
+            this.labelDestinoContent.AutoSize = true;
+            this.labelDestinoContent.Location = new System.Drawing.Point(477, 169);
+            this.labelDestinoContent.Name = "labelDestinoContent";
+            this.labelDestinoContent.Size = new System.Drawing.Size(38, 15);
+            this.labelDestinoContent.TabIndex = 15;
+            this.labelDestinoContent.Text = "label4";
+            // 
+            // labelFecha
+            // 
+            this.labelFecha.AutoSize = true;
+            this.labelFecha.Location = new System.Drawing.Point(610, 71);
+            this.labelFecha.Name = "labelFecha";
+            this.labelFecha.Size = new System.Drawing.Size(38, 15);
+            this.labelFecha.TabIndex = 16;
+            this.labelFecha.Text = "Fecha";
+            // 
+            // labelFechaContent
+            // 
+            this.labelFechaContent.AutoSize = true;
+            this.labelFechaContent.Location = new System.Drawing.Point(713, 71);
+            this.labelFechaContent.Name = "labelFechaContent";
+            this.labelFechaContent.Size = new System.Drawing.Size(38, 15);
+            this.labelFechaContent.TabIndex = 17;
+            this.labelFechaContent.Text = "label2";
+            // 
+            // MoveFormInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelFechaContent);
+            this.Controls.Add(this.labelFecha);
+            this.Controls.Add(this.labelDestinoContent);
+            this.Controls.Add(this.labelDestino);
+            this.Controls.Add(this.labelOrigenContent);
+            this.Controls.Add(this.labelOrigen);
+            this.Controls.Add(this.labelTipoContent);
+            this.Controls.Add(this.labelTipo);
+            this.Controls.Add(this.labelDireccionContent);
+            this.Controls.Add(this.LabelDireccion);
+            this.Controls.Add(this.labelContactoContent);
+            this.Controls.Add(this.labelContacto);
+            this.Controls.Add(this.labelNombreContent);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "MoveFormInformation";
             this.Text = "MoveFormInformation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelNombreContent;
+        private System.Windows.Forms.Label labelContacto;
+        private System.Windows.Forms.Label labelContactoContent;
+        private System.Windows.Forms.Label LabelDireccion;
+        private System.Windows.Forms.Label labelDireccionContent;
+        private System.Windows.Forms.Label labelTipo;
+        private System.Windows.Forms.Label labelTipoContent;
+        private System.Windows.Forms.Label labelOrigen;
+        private System.Windows.Forms.Label labelOrigenContent;
+        private System.Windows.Forms.Label labelDestino;
+        private System.Windows.Forms.Label labelDestinoContent;
+        private System.Windows.Forms.Label labelFecha;
+        private System.Windows.Forms.Label labelFechaContent;
     }
 }
