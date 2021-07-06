fb11abd8406b124f7c2439415c9650dd3b943c82
Julio0204 <pjuliogf0204@gmail.com>
2021-07-05 22:42:45 -0400

    [add]La vista para product information

---
+++ modulo_inventario/modulo_inventario/ProductFormInformation.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace modulo_inventario
+{
+    partial class ProductFormInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelname = new System.Windows.Forms.Label();
+            this.labelidcontenido = new System.Windows.Forms.Label();
+            this.labelcodigo = new System.Windows.Forms.Label();
+            this.labelcantidad = new System.Windows.Forms.Label();
+            this.labelprecioventa = new System.Windows.Forms.Label();
+            this.labelpreciocompra = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(577, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Editar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre del Producto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Codigo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Precio de venta:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(374, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Precio de compra:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Cantidad:";
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.Location = new System.Drawing.Point(173, 162);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(38, 15);
+            this.labelname.TabIndex = 11;
+            this.labelname.Text = "label8";
+            // 
+            // labelidcontenido
+            // 
+            this.labelidcontenido.AutoSize = true;
+            this.labelidcontenido.Location = new System.Drawing.Point(134, 203);
+            this.labelidcontenido.Name = "labelidcontenido";
+            this.labelidcontenido.Size = new System.Drawing.Size(38, 15);
+            this.labelidcontenido.TabIndex = 12;
+            this.labelidcontenido.Text = "label9";
+            // 
+            // labelcodigo
+            // 
+            this.labelcodigo.AutoSize = true;
+            this.labelcodigo.Location = new System.Drawing.Point(111, 249);
+            this.labelcodigo.Name = "labelcodigo";
+            this.labelcodigo.Size = new System.Drawing.Size(44, 15);
+            this.labelcodigo.TabIndex = 13;
+            this.labelcodigo.Text = "label10";
+            // 
+            // labelcantidad
+            // 
+            this.labelcantidad.AutoSize = true;
+            this.labelcantidad.Location = new System.Drawing.Point(126, 291);
+            this.labelcantidad.Name = "labelcantidad";
+            this.labelcantidad.Size = new System.Drawing.Size(44, 15);
+            this.labelcantidad.TabIndex = 14;
+            this.labelcantidad.Text = "label11";
+            // 
+            // labelprecioventa
+            // 
+            this.labelprecioventa.AutoSize = true;
+            this.labelprecioventa.Location = new System.Drawing.Point(505, 164);
+            this.labelprecioventa.Name = "labelprecioventa";
+            this.labelprecioventa.Size = new System.Drawing.Size(44, 15);
+            this.labelprecioventa.TabIndex = 15;
+            this.labelprecioventa.Text = "label12";
+            // 
+            // labelpreciocompra
+            // 
+            this.labelpreciocompra.AutoSize = true;
+            this.labelpreciocompra.Location = new System.Drawing.Point(509, 251);
+            this.labelpreciocompra.Name = "labelpreciocompra";
+            this.labelpreciocompra.Size = new System.Drawing.Size(44, 15);
+            this.labelpreciocompra.TabIndex = 16;
+            this.labelpreciocompra.Text = "label13";
+            // 
+            // ProductFormInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelpreciocompra);
+            this.Controls.Add(this.labelprecioventa);
+            this.Controls.Add(this.labelcantidad);
+            this.Controls.Add(this.labelcodigo);
+            this.Controls.Add(this.labelidcontenido);
+            this.Controls.Add(this.labelname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ProductFormInformation";
+            this.Text = "ProductFormInformation";
+            this.Load += new System.EventHandler(this.ProductFormInformation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.Label labelidcontenido;
+        private System.Windows.Forms.Label labelcodigo;
+        private System.Windows.Forms.Label labelcantidad;
+        private System.Windows.Forms.Label labelprecioventa;
+        private System.Windows.Forms.Label labelpreciocompra;
+    }
+}
---
+++ modulo_inventario/modulo_inventario/ProductFormInformation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using modulo_inventario.models;
+
+namespace modulo_inventario
+{
+    public partial class ProductFormInformation : Form
+    {
+        private Product product;
+
+        public ProductFormInformation()
+        {
+            InitializeComponent();
+        }
+        public ProductFormInformation(Product product)
+        {
+            InitializeComponent();
+            this.product = product;
+
+        }
+        private void loiddata()
+        {
+            labelname.Text = product.Name;
+            labelidcontenido.Text = product.Id.ToString();
+            labelcodigo.Text = product.Code;
+            labelcantidad.Text = product.Available_qty.ToString();
+            labelprecioventa.Text = product.Sales_price.ToString();
+            labelpreciocompra.Text = product.Purchase_price.ToString();
+
+        }
+
+
+        private void ProductFormInformation_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vatras vtr = new vatras();
+            vtr.Show();
+            this.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProductsListView vl = new ProductsListView();
+            vl.Show();
+            this.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vatras vtr = new vatras(product);
+            vtr.Show();
+            this.Visible = false;
+        }
+    }
+}
